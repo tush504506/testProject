2c2c6ddf13e00dee3cb00f7dfd251f55dfc4a0e3
tush504506 <tusharingavale8@gmail.com>
2024-05-08 11:42:28 +0530

    testProject

---
+++ tusharTest/src/main/java/testPackage/testClass.java
@@ -0,0 +1,35 @@
+package testPackage;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String OriginalName="AkshaY";
+		int Length = OriginalName.length();
+		char chh;
+		
+		String ReverseName="";
+		
+		
+		
+		for(int i=0; i<Length; i++){
+			
+		char ch = OriginalName.charAt(i);
+		if(Character.isLowerCase(ch)){
+			chh = Character.toUpperCase(ch);
+			ReverseName=ReverseName+chh;
+		}
+		if (Character.isUpperCase(ch)) {
+			chh = Character.toLowerCase(ch);
+			ReverseName=ReverseName+chh;
+		}		
+	
+	    }
+		
+       System.out.println(ReverseName);
+		
+		
+	}
+
+}
---
+++ tusharTest/src/main/java/testPackage2/testClass2.java
@@ -0,0 +1,11 @@
+package testPackage2;
+
+public class testClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
